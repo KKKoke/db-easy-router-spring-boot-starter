ed192738433b4b164998b15a31602ee53c612067
kkkoke <1120746320@qq.com>
2023-07-15 11:06:25 +0800

    init: 初始化项目

---
+++ src/main/java/com/kkkoke/middleware/db/router/DbEasyRouterSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.kkkoke.middleware.db.router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbEasyRouterSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbEasyRouterSpringBootStarterApplication.class, args);
+    }
+
+}
